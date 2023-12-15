bbb788fac5fc24956085e0c065f990b7c299d4c4
Khanh Vi Pham <KhanhVi.Pham@Student.HTW-Berlin.de>
2023-12-15 12:23:35 +0100

    M4: connect Backend with Frontend

--- src/main/java/htwberlin/webtech/m2/UserService.java
+++ src/main/java/htwberlin/webtech/m2/UserService.java
@@ -31,6 +31,7 @@ public class UserService {
         return userRepository.findById(id).map(user -> {
             user.setUsername(updatedUser.getUsername());
             user.setEmail(updatedUser.getEmail());
+            user.setPassword(updatedUser.getPassword());
             return userRepository.save(user);
         }).orElseThrow(() -> new RuntimeException());
     }
