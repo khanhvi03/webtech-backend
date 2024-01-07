a614f657d2d25e632cc396326bc80d92c2452093
Khanh Vi Pham <KhanhVi.Pham@Student.HTW-Berlin.de>
2024-01-07 22:12:00 +0100

    add H2 DB

--- src/main/java/htwberlin/webtech/m2/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package htwberlin.webtech.m2;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-}
